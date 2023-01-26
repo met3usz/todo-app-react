cb12de2e868d4e5be2e9c44e5110c1836d81a7cf
Mateusz <m.wasik1303@gmail.com>
2023-01-26 23:49:49 +0100

    Added React Fragment

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,18 +1,22 @@
+import React from 'react';
 import './style.css';
 
-const Buttons = ({ tasks, hide }) =>
-  tasks.length > 0 && (
-    <div className="buttons">
-      <button className="button__button">
-        {hide ? 'Pokaż' : 'Ukryj'} wykonane
-      </button>
-      <button
-        className="button__button"
-        disabled={tasks.every(({ done }) => done)}
-      >
-        Ukończ wszystkie
-      </button>
-    </div>
-  );
+const Buttons = ({ tasks, hide }) => (
+  <div className="buttons">
+    {tasks.length > 0 && (
+      <>
+        <button className="button__button">
+          {hide ? 'Pokaż' : 'Ukryj'} wykonane
+        </button>
+        <button
+          className="button__button"
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Ukończ wszystkie
+        </button>
+      </>
+    )}
+  </div>
+);
 
 export default Buttons;
