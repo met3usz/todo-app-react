7b0145c4a52086fceb58febe9dcd6c544d1681b6
Mateusz <m.wasik1303@gmail.com>
2023-01-28 10:37:36 +0100

    UseState testing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import React, { useState } from 'react';
 import Container from './Container';
-import Wrapper from './Wrapper';
-import Section from './Section';
-import List from './List';
-import Buttons from './Buttons';
-import Form from './Form';
+// import Wrapper from './Wrapper';
+// import Section from './Section';
+// import List from './List';
+// import Buttons from './Buttons';
+// import Form from './Form';
 
-const tasks = [
-  { id: 1, content: 'zrobić zakupy', done: false },
-  { id: 2, content: 'zjeść kolację', done: true },
-  { id: 3, content: 'iść spać wcześnie', done: false },
-];
-let hideDoneTasks = false;
+// const tasks = [
+//   { id: 1, content: 'zrobić zakupy', done: false },
+//   { id: 2, content: 'zjeść kolację', done: true },
+//   { id: 3, content: 'iść spać wcześnie', done: false },
+// ];
+//
+// let hideDoneTasks = false;
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <Container>
-      <Wrapper body={<Form />} title={'Lista zadań'} />
+      {/* <Wrapper body={<Form />} title={'Lista zadań'} />
       <Section
         tasks={tasks}
         hideDoneTasks={hideDoneTasks}
         contentControlButtons={<Buttons tasks={tasks} hide={hideDoneTasks} />}
         tasksList={<List tasks={tasks} hide={hideDoneTasks} key={tasks.id} />}
-      />
+      /> */}
+
+      <p>Licznik {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </Container>
   );
 }
