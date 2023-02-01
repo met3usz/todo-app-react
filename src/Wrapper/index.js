7ea5bfbbbb157624fa15152f66ad5bb8e20120c9
Mateusz <m.wasik1303@gmail.com>
2023-02-01 22:13:33 +0100

    Deleted last className

--- src/Wrapper/index.js
+++ src/Wrapper/index.js
@@ -3,7 +3,7 @@ import { TitleWrapper } from './styled';
 const Wrapper = ({ body, title }) => (
   <TitleWrapper>
     <h1>{title}</h1>
-    <h2 className="wrapper__header">Dodaj nowe zadanie</h2>
+    <h2>Dodaj nowe zadanie</h2>
     {body}
   </TitleWrapper>
 );
