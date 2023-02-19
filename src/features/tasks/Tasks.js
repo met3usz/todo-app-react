862c94f5dbe05402d2c36f215bd452cfe4fc1b0c
Mateusz <m.wasik1303@gmail.com>
2023-02-19 01:13:16 +0100

    App split into features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useTasks } from '../../utils/useTasks';
+import { Container } from '../../common/Container/styled';
+import Wrapper from './Wrapper';
+import Section from './Section';
+import List from './List';
+import StyledButtons from './StyledButtons';
+import Form from './Form';
+
+function Tasks() {
+  const [hideDone, setHide] = useState(false);
+
+  const toggleHide = () => {
+    setHide((hideDone) => !hideDone);
+  };
+
+  const { tasks, toggleTaskDone, setAllDone, addNewTask, removeTask } =
+    useTasks();
+
+  return (
+    <Container>
+      <Wrapper body={<Form addNewTask={addNewTask} />} title={'Lista zadań'} />
+      <Section
+        tasks={tasks}
+        hideDone={hideDone}
+        contentControlButtons={
+          <StyledButtons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHide={toggleHide}
+            setAllDone={setAllDone}
+          />
+        }
+        tasksList={
+          <List
+            tasks={tasks}
+            hideDone={hideDone}
+            key={tasks.id}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { App } from './App';
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'addTask':
+      return {
+        ...state,
+        tasks: [...state.tasks, { content: action.payload }],
+      };
+    default:
+      return state;
+  }
+};
+
+const addTask = (content) => ({
+  type: 'addTask',
+  payload: content,
+});
+
+const selectTasks = ({ tasks }) => tasks;
+
+const store = configureStore({ reducer: tasksReducer });
+console.log(selectTasks(store.getState()));
+
+store.dispatch(addTask('zadanie 1'));
+console.log(selectTasks(store.getState()));
+
+store.dispatch(addTask('zadanie 2'));
+console.log(selectTasks(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
