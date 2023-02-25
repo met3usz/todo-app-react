6e7d899325035335e00af7b059354b2f6d295bce
Mateusz <m.wasik1303@gmail.com>
2023-02-25 13:52:10 +0100

    Small title change

--- src/features/tasks/Wrapper/index.js
+++ src/features/tasks/Wrapper/index.js
@@ -1,11 +1,14 @@
 import { Title, TitleWrapper } from './styled';
 
 const Wrapper = ({ body, title }) => (
-  <TitleWrapper>
+  <>
     <Title>{title}</Title>
-    <h2>Dodaj nowe zadanie</h2>
-    {body}
-  </TitleWrapper>
+
+    <TitleWrapper>
+      <h2>Dodaj nowe zadanie</h2>
+      {body}
+    </TitleWrapper>
+  </>
 );
 
 export default Wrapper;
